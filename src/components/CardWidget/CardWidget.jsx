70f81126cb0582b55588539fecf4e4d2ca16473c
caceresnb <84637134+caceresnb@users.noreply.github.com>
2022-04-20 17:51:12 -0300

    Add files via upload

---
+++ src/components/CardWidget/CardWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
+
+const CardWidget = () => {
+    return (
+    <h3>  <FaShoppingCart/>  5 </h3>
+    )
+  }
+export default CardWidget 
