db7d42693cfe0347c10ab22ca0129d710ea09816
caceresnb <84637134+caceresnb@users.noreply.github.com>
2022-04-20 17:50:16 -0300

    Delete src/Components directory

--- src/Components/CardWidget/CardWidget.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { FBsCartFill } from 'react-icons/fa';
-
-const CardWidget = () => {
-    return (
-    <h3>  <FBsCartFill />  5 </h3>
-    )
-  }
-export default CardWidget 
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++
@@ -1,7 +0,0 @@
-const ItemListContainer = ({greeting}) => {
-    return (
-        <div className= 'container bg-primary p-10'>
-            <h2>{greeting}</h2></div>
-    )
-}
-export default ItemListContainer 
