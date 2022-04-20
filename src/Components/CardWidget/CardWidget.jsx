a5976bc3e064625687aef8ed7c2f51f867169be4
caceresnb <84637134+caceresnb@users.noreply.github.com>
2022-04-19 22:03:47 -0300

    Add files via upload

---
+++ src/Components/CardWidget/CardWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { FBsCartFill } from 'react-icons/fa';
+
+const CardWidget = () => {
+    return (
+    <h3>  <FBsCartFill />  5 </h3>
+    )
+  }
+export default CardWidget 
