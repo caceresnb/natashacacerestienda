9d9d619627377535ce937a55910a53d199cffa7f
caceresnb <84637134+caceresnb@users.noreply.github.com>
2022-04-23 18:27:47 -0300

    Delete src directory

--- src/components/CardWidget/CardWidget.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { FaShoppingCart } from 'react-icons/fa';
-
-const CardWidget = () => {
-    return (
-    <h3>  <FaShoppingCart/>  5 </h3>
-    )
-  }
-export default CardWidget 
